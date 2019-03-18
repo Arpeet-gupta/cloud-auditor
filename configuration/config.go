172faed00fed2b0af050b0340ffd7227d9ed5d23
Abhishek <abhishek@example.com>
2019-03-18 23:25:43 +0530

    Added Config Structure for configuration

--- configuration/config.go
+++ configuration/config.go
@@ -5,3 +5,12 @@ import (
 	"github.com/iamabhishek-dubey/cloud-auditor/session/client"
 	"github.com/iamabhishek-dubey/cloud-auditor/logger"
 )
+
+type Config struct {
+	Regions        *[]string
+	Services       *[]string
+	Profile        string
+	SessionFactory *sessionfactory.SessionFactory
+	ClientFactory  clientfactory.ClientFactory
+	Logger         *logger.Logger
+}
