2395a53b6e734aea2d97904fcb0c064c5e5dd49f
Abhishek <abhishek@example.com>
2019-03-18 23:22:53 +0530

    Added configuration folder

---
+++ configuration/config.go
@@ -0,0 +1,7 @@
+package configuration
+
+import (
+	"github.com/iamabhishek-dubey/cloud-auditor/session/factory"
+	"github.com/iamabhishek-dubey/cloud-auditor/session/client"
+	"github.com/iamabhishek-dubey/cloud-auditor/logger"
+)
