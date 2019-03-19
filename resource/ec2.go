8951bfec433a45608beff9f79c9072224d75a4dd
Abhishek <abhishek@example.com>
2019-03-19 16:47:05 +0530

    Added function for ec2 resource

---
+++ resource/ec2.go
@@ -0,0 +1,38 @@
+package resource
+
+import (
+	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
+	"github.com/iamabhishek-dubey/cloud-auditor/session"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+type Ec2s []*ec2.Instance
+
+func (e *Ec2s) LoadFromAWS(config *configuration.Config, region string) error {
+	ec2API, err := config.ClientFactory.GetEc2Client(csasession.SessionConfig{Profile: config.Profile, Region: region})
+	if err != nil {
+		return err
+	}
+
+	q := &ec2.DescribeInstancesInput{}
+	for {
+		result, err := ec2API.DescribeInstances(q)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+					case "OptInRequired":
+						config.Logger.Warning("you are not subscribed to the EC2 service in region: " + region)
+						break
+					default:
+						return err
+				}
+			} else {
+				return err
+			}
+		}
+	}
+	for _, reservation := range result.Reservations {
+		*e = append(*e, reservation.Instances...)
+	}
+}
