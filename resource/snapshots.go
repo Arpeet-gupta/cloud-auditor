6e33f9d061c8ad6b7502fa2c47d37233a4e63aa1
Abhishek <abhishek@example.com>
2019-03-19 20:13:23 +0530

    Added code for snapshots functions

---
+++ resource/snapshots.go
@@ -0,0 +1,10 @@
+package resource
+
+import (
+	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
+	"github.com/iamabhishek-dubey/cloud-auditor/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
