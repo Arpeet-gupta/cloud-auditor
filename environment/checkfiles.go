96b7a1ce25f7e39dd05804ba351cc133159ede58
Abhishek <abhishek@example.com>
2019-03-20 15:08:34 +0530

    Added import funstion

--- environment/checkfiles.go
+++ environment/checkfiles.go
@@ -1,1 +1,11 @@
 package environment
+
+import (
+	"bufio"
+	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
+	"github.com/iamabhishek-dubey/cloud-auditor/helpers"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"os"
+	"strconv"
+	"strings"
+)
