015b5741406429c36895c18323939bac0741c291
Abhishek <abhishek@example.com>
2019-03-21 15:35:50 +0530

    Added Run function for scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -6,3 +6,8 @@ import (
 	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
 	"github.com/iamabhishek-dubey/cloud-auditor/report"
 )
+
+func Run(config *configuration.Config) error {
+
+}
+
