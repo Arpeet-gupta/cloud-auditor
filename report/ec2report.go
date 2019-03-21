13a907024be4fde03f8632db18b1e436cce23c46
Abhishek <abhishek@example.com>
2019-03-21 12:41:34 +0530

    Added file for import

--- report/ec2report.go
+++ report/ec2report.go
@@ -1,2 +1,11 @@
 package report
 
+import (
+	"bytes"
+	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
+	"github.com/iamabhishek-dubey/cloud-auditor/environment"
+	"github.com/iamabhishek-dubey/cloud-auditor/resource"
+	"sort"
+	"strconv"
+	"strings"
+)
