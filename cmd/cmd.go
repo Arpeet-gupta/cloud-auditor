46e52248b69103eb1a0d4f5e9791b658c9173fbc
Abhishek <abhishek@example.com>
2019-03-21 15:43:24 +0530

    Added cmd file and import function

---
+++ cmd/cmd.go
@@ -0,0 +1,11 @@
+package cmd
+
+import (
+	"os"
+	"github.com/iamabhishek-dubey/cloud-auditor/configuration"
+	"github.com/iamabhishek-dubey/cloud-auditor/session"
+	"github.com/iamabhishek-dubey/cloud-auditor/environment"
+	"github.com/iamabhishek-dubey/cloud-auditor/resource"
+	"github.com/iamabhishek-dubey/cloud-auditor/scanner"
+	"github.com/spf13/cobra"
+)
