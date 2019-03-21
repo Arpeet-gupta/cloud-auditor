bcd895455542d8fc46b1fc9193ebcd214100bfda
Abhishek <abhishek@example.com>
2019-03-21 15:44:09 +0530

    Adde vars for root CMD

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -9,3 +9,11 @@ import (
 	"github.com/iamabhishek-dubey/cloud-auditor/scanner"
 	"github.com/spf13/cobra"
 )
+
+// var cfgFile string
+var config = configuration.GetConfig()
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cloud-security-audit",
+}
