b5e0d1a1d932e47c8282f4cc1694902f83533b1a
Abhishek <abhishek@example.com>
2019-03-21 12:19:00 +0530

    Added function for sort table and ec2 tags

--- report/sortabletags.go
+++ report/sortabletags.go
@@ -17,5 +17,12 @@ func NewSortableTags() *SortableTags {
 }
 
 func (st *SortableTags) Add(tags []*ec2.Tag) {
-
+	for _, tag := range tags {
+		st.Keys = append(st.Keys, *tag.Key)
+		st.Tags[*tag.Key] = *tag.Value
+	}
+	less := func(i, j int) bool {
+		return strings.ToLower(st.Keys[i]) < strings.ToLower(st.Keys[j])
+	}
+	sort.Slice(st.Keys, less)
 }
