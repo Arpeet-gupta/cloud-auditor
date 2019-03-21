bc052a57aafe13b535cd5dd84b19566e9ee22afd
Abhishek <abhishek@example.com>
2019-03-21 15:38:12 +0530

    Completed Run function for scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -10,8 +10,19 @@ import (
 func Run(config *configuration.Config) error {
 	for _, service := range *config.Services {
 		switch strings.ToLower(service) {
-			
+			case "ec2":
+				config.Logger.Info("Gathering information about EC2s...")
+				ec2Reports := report.Ec2Reports{}
+				resources, err := ec2Reports.GetResources(config)
+				if err != nil {
+					return err
+				}
+				ec2Reports.GenerateReport(resources)
+				report.PrintTable(&ec2Reports)
+			default:
+				return fmt.Errorf("Wrong service name: %s", service)
 		}
 	}
+	return nil
 }
 
